ed7ab9bd7c50001c6183aeebda53e7f24e2844a3
bryanpijohnson <47281061+bryanpijohnson@users.noreply.github.com>
2024-01-03 20:38:20 -0500

    working on queries for the assignment

---
+++ EmployeeSQL/SQL Queries.sql
@@ -0,0 +1,34 @@
+-- Active: 1704240461407@@localhost@5432@Employees@public
+
+-- Question 1: List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no AS employee_number, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+INNER JOIN salaries AS s
+ON e.emp_no = s.emp_no;
+
+-- Question 2: List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- Question 3: List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT d.dept_name, d.dept_no, e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+INNER JOIN dept_manager AS dm
+ON e.emp_no = dm.emp_no
+INNER JOIN departments AS d
+ON dm.dept_no = d.dept_no;
+
+-- Question 4: List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+
+-- Question 5: List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+
+-- Question 6: List each employee in the Sales department, including their employee number, last name, and first name.
+
+
+-- Question 7: List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- Question 8: List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
