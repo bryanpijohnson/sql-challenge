6624f53a2c41d3e10d30b1502f80b11a64fb4cb4
bryanpijohnson <47281061+bryanpijohnson@users.noreply.github.com>
2024-01-04 18:32:44 -0500

    added schema SQL file and finished questions

--- EmployeeSQL/SQL Queries.sql
+++ EmployeeSQL/SQL Queries.sql
@@ -33,9 +33,25 @@ FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- Question 6: List each employee in the Sales department, including their employee number, last name, and first name.
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
 
 -- Question 7: List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
 
 
 -- Question 8: List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name, COUNT(*) AS count
+FROM employees
+GROUP BY last_name;
